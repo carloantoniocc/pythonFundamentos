34657da6a5d6de12fe7f3e613136fc6a326c7192
Your Name <you@example.com>
2023-09-04 21:43:57 -0300

    Clase variables

---
+++ 02_vars.py
@@ -0,0 +1,11 @@
+# Variables
+my_name = "carlos"
+print(my_name)
+
+my_age = 42
+print(my_age)
+
+#input
+
+my_name = input("Cual es tu nombre ? ")
+print("Tu nombre es : ", my_name)
---
+++ main.py
@@ -0,0 +1,9 @@
+# comentarios
+'''
+esto igual es un comentario
+pero con mas lineas
+'''
+# python siempre ejecuta main.py al comienzo
+print("hola mundo")
+
+# variables
